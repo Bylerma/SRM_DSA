e3349bfe0a47159402c1d0861c7da6aa6de61f71
Bhanu Pratap Singh <68643355+Bylerma@users.noreply.github.com>
2026-06-17 11:58:09 +0530

    Add solution for 3614. Process String with Special Operations II

---
+++ solutions/2026-06-17/3614-process-string-with-special-operations-ii/solution.py
@@ -0,0 +1,42 @@
+# Number: 3614
+# Title: Process String with Special Operations II
+# Difficulty: Hard
+# Tags: String, Simulation
+# Language: Python3
+# URL: https://leetcode.com/problems/process-string-with-special-operations-ii/
+# Date: 2026-06-17
+# Runtime: 0ms
+# Memory: 0.00MB
+
+class Solution:
+     def processStr(self, s: str, k: int) -> str:
+      length = 0
+      for c in s:
+          if c == "*":
+              if length:
+                  length -= 1
+          elif c == "#":
+              length *= 2
+          elif c == "%":
+              pass
+          else:
+              length += 1
+      if k + 1 > length:
+          return "."
+      for c in reversed(s):
+          if c == "*":
+              length += 1
+          elif c == "#":
+              if k + 1 > (length + 1) // 2:
+                  k -= length // 2
+              length = (length + 1) // 2
+          elif c == "%":
+              k = length - k - 1
+          else:
+              if k + 1 == length:
+                  return c
+              length -= 1
+      return "."
+
+
+
