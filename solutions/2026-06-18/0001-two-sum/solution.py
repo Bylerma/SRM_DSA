adfead7fd77ee68d658ba925d32842b5f4dbd2ea
Bhanu Pratap Singh <68643355+Bylerma@users.noreply.github.com>
2026-06-18 11:24:09 +0530

    Add solution for 1. Two Sum

--- solutions/2026-06-18/0001-two-sum/solution.py
+++ solutions/2026-06-18/0001-two-sum/solution.py
@@ -5,17 +5,13 @@
 # Language: Python3
 # URL: https://leetcode.com/problems/two-sum/
 # Date: 2026-06-18
-# Runtime: 3ms
+# Runtime: 3ms (Beats 100%)
 # Memory: N/A
 
 class Solution:
-    def twoSum(self, nums, target):
-        d = {}
-
-        for i, x in enumerate(nums):
-            need = target - x
-
-            if need in d:
-                return [d[need], i]
-
-            d[x] = i
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
